e6e7f095f630926e91564c4da93143717ebbba6b
Osama <107386980+osSidd@users.noreply.github.com>
2023-12-31 12:35:49 +0530

    clean slate

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import Navbar from './components/navbar'
-import Hero from './components/hero/hero'
-import Services from './components/services/services'
-import Testimonial from './components/testimonial/testimonial'
-import Signup from './components/signup/signup'
-import Footer from './components/footer'
-
-function App(){
-
-  return(
-      <div>
-        {/* navbar */}
-        <Navbar/>
-
-        {/* hero section */}
-        <Hero/>
-        
-        {/* services section */}
-        <Services/>
-        
-        {/* testimonial section */}
-        <Testimonial/>
-        
-        {/* sign up section */}
-        <Signup/>
-        
-        {/* footer */}
-        <Footer/>
-      </div>
-   
+export default function App(){
+  return (
+    <div>
+      
+    </div>
   )
 }
-export default App
--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(){
-    return (
-        <div className='bg-footer-bg py-6'>
-            <div className='wrapper text-center text-footer-text text-sm'>
-                Copyright &copy; {new Date().getFullYear()} Launchpad by <a href="https://github.com/osSidd" target="_blank" className='text-blue-400 font-semibold'>osSidd</a>
-            </div>
-        </div>
-    )
-}
