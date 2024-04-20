9a43d9e1039c74441f993f1b07f162a32761d061
Osama <107386980+osSidd@users.noreply.github.com>
2024-04-20 13:12:55 +0530

    fixed overflow in footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Specifications from "./components/specs/specification";
 
 export default function App(){
   return (
-    <div className=''>
+    <div className='w-full'>
       <header>
         <Navbar/>
         <Hero/>
--- src/components/footer/copyright.jsx
+++ src/components/footer/copyright.jsx
@@ -5,15 +5,15 @@ export default function Copyright(){
     const linkClassStr = `
             text-copyright-links lg:font-semibold 
             underline-offset-4 decoration-2 hover:underline 
-            transition-all duration-1000 ease-in-out md:whitespace-nowrap text-copyright-links-mobile md:text-xs
+            transition-all duration-1000 ease-in-out lg:whitespace-nowrap text-copyright-links-mobile md:text-xs
         `
 
     return(
-        <section className=' h-80 lg:h-auto  flex flex-col lg:flex-row items-center lg:justify-between px-4 py-8 bg-footer-bg'>
-            <small className='md:w-7/12 lg:w-5/12 xl:w-6/12 text-center lg:text-left text-copyright-address text-copyright-links-mobile md:text-sm lg:text-xs'>
+        <section className='w-full h-80 lg:h-auto  flex flex-col lg:flex-row items-center lg:justify-between px-4 py-8 bg-footer-bg'>
+            <small className='md:w-5/12 xl:w-6/12 text-center lg:text-left text-copyright-address text-copyright-links-mobile md:text-sm lg:text-xs'>
                 &copy; Nintendo. Games are property of their respective owners. Nintendo of America Inc. Headquarters are in Redmond, Washington, USA
             </small>
-            <div className='mt-4 lg:mt-0 flex flex-row justify-center items-center md:w-6/12'>
+            <div className='mt-4 lg:mt-0 flex flex-row justify-end items-center lg:w-7/12'>
                 {
                     CopyrightLinks.map(link => (
                         <small key={link.href} className='ml-4 mt-2 md:mt-0'>
@@ -33,7 +33,7 @@ export default function Copyright(){
                         src='https://assets.nintendo.com/image/upload/c_scale,w_24,q_auto/ncom/global/flags-change-region/FlagUsaIconRegionSelect.webp' 
                         alt="region" 
                     />
-                    <small className={linkClassStr + 'decoration-white ml-2 '}>
+                    <small className={linkClassStr + 'decoration-white ml-2'}>
                         English (United States)
                     </small>
                 </a>
