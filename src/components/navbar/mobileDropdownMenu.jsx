2d276c9faadb80ebb9a109e8f50bc605c8a20f2c
Osama <107386980+osSidd@users.noreply.github.com>
2024-04-26 22:32:11 +0530

    added submenu page

--- src/components/navbar/mobileDropdownMenu.jsx
+++ src/components/navbar/mobileDropdownMenu.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Angle from "../icons/angle";
 import Close from "../icons/close";
 import Games from "../icons/games";
@@ -11,7 +12,24 @@ export default function MobileDropdownMenu({toggleMobileMenu}){
 
     const language = 'https://assets.nintendo.com/image/upload/c_scale,w_24,q_auto/ncom/global/flags-change-region/FlagUsaIconRegionSelect.webp'
 
+    const [displaySubMenu, setDisplaySubMenu] = useState({
+        display: false,
+        content: ''
+    })
+
+    function toggleSubMenu(state, label){
+        setDisplaySubMenu(prev => ({
+            ...prev,
+            display: state,
+            content: label
+        }))
+    }
+
     return (
+        displaySubMenu.display ? 
+        
+        <SubMenuPage toggleMobileMenu={toggleMobileMenu} toggleSubMenu={toggleSubMenu} displaySubMenu={displaySubMenu}/> : 
+        
         <div className=" bg-nav-info rounded-t-3xl pb-8 overflow-clip">
             <div className="flex items-center justify-center px-4 py-6 border-b border-gray-300 bg-white">
                 <h2 className="ml-auto font-semibold text-lg">Menu</h2>
@@ -20,11 +38,11 @@ export default function MobileDropdownMenu({toggleMobileMenu}){
                 </div>
             </div>
             <div className="max-h-72 overflow-y-auto">
-                <DropdownItems Icon={<Store color='#e60012'/>} label='My Nintendo Store' drawer={true}/>
-                <DropdownItems Icon={<Games color='#e60012'/>} label='Games' drawer={true}/>
-                <DropdownItems Icon={<Switch color='#e60012'/>} label='Nintendo Switch' drawer={true}/>
+                <DropdownItems toggleSubMenu={toggleSubMenu} Icon={<Store color='#e60012'/>} label='My Nintendo Store' drawer={true}/>
+                <DropdownItems toggleSubMenu={toggleSubMenu} Icon={<Games color='#e60012'/>} label='Games' drawer={true}/>
+                <DropdownItems toggleSubMenu={toggleSubMenu} Icon={<Switch color='#e60012'/>} label='Nintendo Switch' drawer={true}/>
                 <DropdownItems Icon={<News color='#e60012'/>} label='News & Events' href='https://www.nintendo.com/us/whatsnew/' drawer={false}/>
-                <DropdownItems Icon={<Play color='#e60012'/>} label='Play Nintendo' drawer={true}/>
+                <DropdownItems toggleSubMenu={toggleSubMenu} Icon={<Play color='#e60012'/>} label='Play Nintendo' drawer={true}/>
                 <div className="mt-4">
                     <DropdownItems Icon={<Support color='#e60012'/>} label='Support' href='https://en-americas-support.nintendo.com/' drawer={false}/>
                     <DropdownItems Icon={<img className="rounded" src={language} alt="language"/>} href='https://www.nintendo.com/us/regionselector/' label='Change Region' drawer={false}/>
@@ -34,18 +52,37 @@ export default function MobileDropdownMenu({toggleMobileMenu}){
     )
 }
 
-function DropdownItems({Icon, label, drawer=false, href=false}){
+function DropdownItems({Icon, label, drawer=false, href=false, toggleSubMenu}){
     const base = 
         <div className="flex items-center px-6 py-4 border-b border-gray-300 bg-white">
             <div className="mr-2">
                 {Icon}
             </div>
             <p className="font-semibold">{label}</p>
-            <div className="ml-auto">
+            <div className="ml-auto" onClick={drawer ? () => {toggleSubMenu(true, label)} : undefined}>
                 {
                     drawer ? <Angle/> : null
                 }
             </div>
         </div>
     return href ? <a href={href}>{base}</a> : base
+}
+
+function SubMenuPage({toggleMobileMenu, toggleSubMenu, displaySubMenu}){
+    return (
+        <div className=" bg-nav-info rounded-t-3xl pb-8 overflow-clip">
+            <div className="flex items-center justify-between px-4 py-6 border-b border-gray-300 bg-white">
+                <div className=" rotate-180  " onClick={() => {toggleSubMenu(false)}}>
+                    <Angle/>
+                </div>
+                <h2 className="font-semibold text-lg">{displaySubMenu.content}</h2>
+                <div className="" onClick={toggleMobileMenu}>
+                    <Close/>
+                </div>
+            </div>
+            <div className="h-72 overflow-y-auto">
+                
+            </div>
+        </div>
+    )
 }
